21d3da04c76d908573b719122e33952449ac7fe2
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2020-07-17 15:34:40 +0700

    Inclue Interface and

---
+++ helpers/localStorage.ts
@@ -0,0 +1,17 @@
+export const getLocalState = (key: any) => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    if (serializedState == null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+export const setLocalState = (key: any, value: any) => {
+  try {
+    const serializedState = JSON.parse(JSON.stringify(value));
+    localStorage.setItem(key, serializedState);
+  } catch (err) {}
+};
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,10 +1,63 @@
-// You can include shared interfaces/types in a separate file
-// and then use them in any component by importing them. For
-// example, to import the interface below do:
-//
-// import User from 'path/to/interfaces';
+export interface Product {
+  id: number;
+  title: string;
+  weight: string;
+  unit: string;
+  categories: [Category];
+  author: string;
+  gallery: [Gallery];
+  price: number;
+  category: string;
+  string: string;
+  inStock?: number;
+  image: string;
+  quantity: number;
+  salePrice: number;
+}
 
-export type User = {
-  id: number
-  name: string
+export interface Category {
+  id: number;
+  title: string;
+  slug: string;
+}
+export interface Gallery {
+  url: string;
+}
+export interface CartProduct extends Product {
+  quantity: number;
+}
+
+export interface RadioDataType {
+  id: number;
+  title: string;
+  value: string;
+  label: string;
+  disabled?: boolean;
+}
+
+export interface CardType {
+  id: number;
+  type: string;
+  cardNumber: string;
+  cardName: string;
+  disabled?: boolean;
+}
+
+export interface PaymentOptionType {
+  showCard?: boolean;
+  addedCard?: CardType[];
+  mobileWallet?: boolean;
+  cashOnDelivery?: boolean;
+}
+
+export interface Progress {
+  checked?: boolean;
+  title: string;
+  subtile?: string;
+}
+
+export interface Coupon {
+  id?: number;
+  code?: string;
+  discountInPercent?: number;
 }
