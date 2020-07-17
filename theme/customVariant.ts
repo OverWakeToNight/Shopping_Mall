461fb1d9f9e17e54b5c6930f2d5f70f4b6d23af4
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2020-07-17 10:05:30 +0700

    Include Library And Struct For Project Shopping Mall

---
+++ theme/customVariant.ts
@@ -0,0 +1,13 @@
+import { variant } from "styled-system";
+const buttonStyle = variant({
+  key: "buttonStyle",
+});
+const colorStyle = variant({
+  key: "colorStyle",
+  prop: "colors",
+});
+const buttonSize = variant({
+  key: "buttonSize",
+  prop: "size",
+});
+export { buttonStyle, colorStyle, buttonSize };
